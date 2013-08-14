4af71f384e5e26afff983da8d6cedb8c15d9067c
Adam Hooper <adam@adamhooper.com>
2013-08-14 12:34:24 -0400

    Add UploadProgress object which watches UploadCollection

---
+++ src/coffee/MassUpload/UploadProgress.coffee
@@ -0,0 +1,53 @@
+define [ 'backbone' ], (Backbone) ->
+  Backbone.Model.extend
+    defaults:
+      loaded: 0
+      total: 0
+
+    initialize: ->
+      collection = @get('collection')
+      throw 'Must initialize UploadProgress with `collection`, an UploadCollection' if !collection?
+
+      adjust = (dLoaded, dTotal) =>
+        @set
+          loaded: @get('loaded') + dLoaded
+          total: @get('total') + dTotal
+        undefined
+
+      cidToLastKnownProgress = {}
+
+      add = (model) ->
+        progress = model.getProgress()
+        adjust(progress.loaded, progress.total)
+        cidToLastKnownProgress[model.cid] = progress
+      remove = (model) ->
+        progress = cidToLastKnownProgress[model.cid]
+        adjust(-progress.loaded, -progress.total)
+        delete cidToLastKnownProgress[model.cid]
+      change = (model) ->
+        oldProgress = cidToLastKnownProgress[model.cid]
+        newProgress = model.getProgress()
+        adjust(newProgress.loaded - oldProgress.loaded, newProgress.total - oldProgress.total)
+        cidToLastKnownProgress[model.cid] = newProgress
+      reset = =>
+        cidToLastKnownProgress = {}
+        progress = { loaded: 0, total: 0 }
+        @get('collection').each (model) ->
+          modelProgress = model.getProgress()
+          cidToLastKnownProgress[model.cid] = modelProgress
+          progress.loaded += modelProgress.loaded
+          progress.total += modelProgress.total
+        @set(progress)
+
+      events =
+        add: add
+        remove: remove
+        change: change
+        reset: reset
+
+      for eventName, callback of events
+        collection.on(eventName, callback)
+
+      reset()
+
+      undefined
---
+++ src/js/MassUpload/UploadProgress.js
@@ -0,0 +1,71 @@
+define(['backbone'], function(Backbone) {
+  return Backbone.Model.extend({
+    defaults: {
+      loaded: 0,
+      total: 0
+    },
+    initialize: function() {
+      var add, adjust, callback, change, cidToLastKnownProgress, collection, eventName, events, remove, reset,
+        _this = this;
+      collection = this.get('collection');
+      if (collection == null) {
+        throw 'Must initialize UploadProgress with `collection`, an UploadCollection';
+      }
+      adjust = function(dLoaded, dTotal) {
+        _this.set({
+          loaded: _this.get('loaded') + dLoaded,
+          total: _this.get('total') + dTotal
+        });
+        return void 0;
+      };
+      cidToLastKnownProgress = {};
+      add = function(model) {
+        var progress;
+        progress = model.getProgress();
+        adjust(progress.loaded, progress.total);
+        return cidToLastKnownProgress[model.cid] = progress;
+      };
+      remove = function(model) {
+        var progress;
+        progress = cidToLastKnownProgress[model.cid];
+        adjust(-progress.loaded, -progress.total);
+        return delete cidToLastKnownProgress[model.cid];
+      };
+      change = function(model) {
+        var newProgress, oldProgress;
+        oldProgress = cidToLastKnownProgress[model.cid];
+        newProgress = model.getProgress();
+        adjust(newProgress.loaded - oldProgress.loaded, newProgress.total - oldProgress.total);
+        return cidToLastKnownProgress[model.cid] = newProgress;
+      };
+      reset = function() {
+        var progress;
+        cidToLastKnownProgress = {};
+        progress = {
+          loaded: 0,
+          total: 0
+        };
+        _this.get('collection').each(function(model) {
+          var modelProgress;
+          modelProgress = model.getProgress();
+          cidToLastKnownProgress[model.cid] = modelProgress;
+          progress.loaded += modelProgress.loaded;
+          return progress.total += modelProgress.total;
+        });
+        return _this.set(progress);
+      };
+      events = {
+        add: add,
+        remove: remove,
+        change: change,
+        reset: reset
+      };
+      for (eventName in events) {
+        callback = events[eventName];
+        collection.on(eventName, callback);
+      }
+      reset();
+      return void 0;
+    }
+  });
+});
